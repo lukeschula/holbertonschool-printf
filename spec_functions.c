03d71a31f51c10749341e3bf67b340f826716845
Your Name <you@example.com>
2023-11-03 12:10:54 -0700

    changed function names to match format_struct.c

--- spec_functions.c
+++ spec_functions.c
@@ -16,20 +16,20 @@ void print_char(va_list args)
 }
 
 /**
- * print_percent - function that prints the % conversion specifier following a %
+ * print_pct - function that prints the % conversion specifier following a %
  */
 
-void print_percent(va_list args)
+void print_pct(va_list args)
 {
 	putchar('%');
 }
 
 /**
- * print_string - function that will print the inputted string following a %s
+ * print_str - function that will print the inputted string following a %s
  * @args: variable argument passed to main function
  */
 
-void print_string(va_list args)
+void print_str(va_list args)
 {
 	char *string;
 
