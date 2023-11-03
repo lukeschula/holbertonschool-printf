fa42321ae30c9eaa57e4460a9f08affb4254cdf6
root <schulaluke@gmail.com>
2023-11-03 11:23:55 -0700

    created function that takes a character pointer and returns a function pointer

---
+++ format_struct.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * check_spec - function pointer to test if specifiers are valid
+ *
+ * @format: pointer to the character, this charatcer will then be tested 
+ * if is a valid specifier or not
+ *
+ * Return: a pointer to a function housing the list of the argumants
+ */
+int(*check_spec(const char, *format))(va_list)
+{
+	int a;
+	
+	form_t b[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"%", print_pct},
+		{"d", print_dec},
+		{"i", print_dec},
+		{NULL, NULL}
+	};
+
+	for (a = 0; b[a].d; a++)
+	{
+		if (*format = *(b[a].d)
+		
+			return (b[a].e)
+		
+	}	
+	return (NULL);
+}
