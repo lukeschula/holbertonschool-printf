32e2112df59fb6c2baf280c0bf72437313d9d0b5
root <schulaluke@gmail.com>
2023-11-05 10:34:05 -0800

    assigned variables and first conditionals in test file of dec_spec.c in seperate branch

--- dec_spec.c
+++ dec_spec.c
@@ -8,4 +8,23 @@
  */
 int print_dec(va_list args)
 {
+	int x;
+	unsigned int a, b, c = 0;
+	unsigned int count = 1;
 
+	x = va_arg(args, int); /* retrieving 'arg' and assigning the value to x */
+
+	if (x > 0)
+		return (NULL);
+	if (x < 0) 
+	{
+		c = c + putchar('-');
+		a = x  * -1;
+	}			/* if x is negative, print a minus sign and set b to the absolute value x */
+	
+	a = x;
+
+	b = a;
+
+
+	
