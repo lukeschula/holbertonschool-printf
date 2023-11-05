c5d10d94151c6ade428830e37d93cd335dcc2a17
root <schulaluke@gmail.com>
2023-11-05 10:11:13 -0800

    created seperate branch to work on dec_spec.c file w/o causing any disruption to the main branch

---
+++ dec_spec.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_dec - identifies i and d specifiers and returns 
+ *
+ * @args: the argument passed through the passed through logic 
+ *
+ * Return: the length of the printed number
+ */
+int print_dec(va_list args)
+{
+
