db44c6c4d2a2debb2756d0b08149a2a0f1b9e32c
Your Name <you@example.com>
2023-11-05 23:44:30 -0800

    made fixes to counter to make sure it is correctly counting length of printed number

--- dec_spec.c
+++ dec_spec.c
@@ -9,15 +9,15 @@
 int print_dec(va_list args)
 {
 	int x;
-	unsigned int a, b, c = 0;
+	unsigned int a, b;
 	unsigned int count = 1;
 
 	x = va_arg(args, int); /* retrieving 'arg' and assigning the value to x */
 
 	if (x < 0) 
 	{
-		c = c + putchar('-');
-		a = x * -1;
+		putchar('-');
+		a = -x;
 	}			/* if x is negative, print a minus sign and set a to the absolute value x */
 	else
 	{
@@ -28,16 +28,14 @@ int print_dec(va_list args)
 
 	while (b > 9)
 	{
-		b = b / 10;
-		count = count * 10; /* finds # of digits in a by dividing b by 10 until < 10 */
-				    /* count used to track # of digits */
+		b = b / 10; /*finds # of digits in 'a' by dividing b by 10 until < 10 */
+		count++; /* increment count for each digit */
 	}
-	while (count >= 1)
+	while (count > 0)
 	{
-		putchar(((a / count) % 10) + '0'); /* prints each digit of a 1 by 1 from left to right */
+		putchar((a / count) % 10 + '0'); /* prints each digit of a 1 by 1 from left to right */
 		count = count / 10; /* reduces count by a factor of 10 to move to next digit */
-		c++; /*increment count for each character printed */
 	}
 	
-	return (c); /* returns the length of the printed number stored in c*/
+	return (count); /* returns the count of digits */
 }
