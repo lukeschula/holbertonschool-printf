0ca17c3a9dec20b053abfa065e2e67ddc0e41dd8
Your Name <you@example.com>
2023-11-06 08:57:40 -0800

    formatting edits

--- dec_spec.c
+++ dec_spec.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * print_dec - identifies i and d specifiers and returns 
- *
- * @args: the argument passed through the logic 
+ * print_dec - identifies i and d specifiers and returns
+ * @args: the argument passed through the logic
  *
  * Return: the length of the printed number
  */
@@ -15,12 +14,15 @@ int print_dec(va_list args)
 
 	x = va_arg(args, int); /* retrieving 'arg' and assigning the value to x */
 
-	if (x < 0) 
+	if (x < 0)
 	{
 		putchar('-');
-		a = -x; /* if x is negative, print a minus sign and set 'a' to absolute value of 'x' */
+		a = -x; /**
+			  *if x is negative, print a minus sign
+			  *sets 'a' to absolute value of 'x'
+			  */
 		length++; /* increment length to account for minus sign */
-	}			
+	}
 	else
 	{
 		a = x; /* don't reassign to a if x is negative */
@@ -30,15 +32,23 @@ int print_dec(va_list args)
 
 	while (b > 9)
 	{
-		b = b / 10; /*finds # of digits in 'a' by dividing b by 10 until < 10 */
-		count = count * 10; /* for each digit removed from b we increase the value of count by 10 */
+		b = b / 10; /**
+			      *finds # of digits in 'a' by dividing b by 10 until < 10
+			      */
+		count = count * 10; /**
+				      *for each digit removed from b we increase the value of count by 10
+				      */
 	}
 	while (count > 0)
 	{
-		putchar((a / count) % 10 + '0'); /* prints each digit of 'a' by converting it to ASCII value */
-		count = count / 10; /*reduces count by factor of 10 to move to next digit */
+		putchar((a / count) % 10 + '0'); /**
+						  *prints each digit of 'a' by converting it to ASCII value
+						  */
+		count = count / 10; /**
+				      *reduces count by factor of 10 to move to next digit
+				      */
 		length++; /* increment length for each digit printed */
 	}
-	
+
 	return (length); /* returns the total length of printed output */
 }
