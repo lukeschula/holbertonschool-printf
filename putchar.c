620ca31dda3699b4a7a6264cc649f38fe8d0ae93
Your Name <you@example.com>
2023-11-06 08:57:50 -0800

    formatting edits

--- putchar.c
+++ putchar.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to print 
+ * @c: the character to print
  *
- * Return: On success, 1. 
+ * Return: On success, 1.
  * On error, -1 is returned
  */
 int _putchar(char c)
